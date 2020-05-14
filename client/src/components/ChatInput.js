2e8d3e63c87baa58edab118c5b26fcc7da95043b
jonfossheim <fossheimjoneven@gmail.com>
2020-05-14 14:29:13 +0200

    prepare message state

--- client/src/components/ChatInput.js
+++ client/src/components/ChatInput.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 const ChatInput = () => {
+    const [message, setMessage] = useState('')
+    const [messageEmpty, setMessageEmpty] = useState(true)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setMessage('')
+
+    }
+
+    const handleChange = (input) => {
+        let value = input.target.value;
+        setMessage(value)
+    }
+
     return(
         <>
         </>
