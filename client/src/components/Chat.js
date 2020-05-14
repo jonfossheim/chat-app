e7ae0d6829e004b5085cce5a185860f681333031
jonfossheim <fossheimjoneven@gmail.com>
2020-05-14 14:37:19 +0200

    prepare name entry and message display

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,9 +1,32 @@
 import React from "react";
+import {TextField, Button} from "@material-ui/core";
+
+import ChatInput from './ChatInput'
+import ChatMessage from './ChatMessage'
 
 const Chat = () => {
-    return(
-        <>
-        </>
+    const [name, setName] = useState('')
+    const [messages, setMessages] = useState([])
+
+    return (
+        <div>
+            <TextField
+                id="name"
+                label="Your name.."
+                name={'name'}
+                value={name}
+                onChange={e => setName(e.target.value)}
+                variant="outlined"
+            />
+            <ChatInput/>
+            {messages.map((message, index) =>
+                <ChatMessage
+                    key={index}
+                    message={message.message}
+                    name={message.name}
+                />,
+            )}
+        </div>
     )
 }
 
