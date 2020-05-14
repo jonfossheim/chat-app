d48161f70b060a90be32ade82b1ab7a85ba4c13e
jonfossheim <fossheimjoneven@gmail.com>
2020-05-14 14:58:10 +0200

    add dummy link

--- client/src/pages/WelcomeScreen.js
+++ client/src/pages/WelcomeScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const WelcomeScreen = () => {
-    return(
+    return (
         <div>
-
+            <h1>Welcome to FossChat!</h1>
+            <Link to="/room">Room</Link>
         </div>
-    )
+)
 }
 
 export default WelcomeScreen
