743da72f6670c4ea187df6e34681e472b2976871
jonfossheim <fossheimjoneven@gmail.com>
2020-05-14 14:39:57 +0200

    add useEffects for websockets

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {TextField, Button} from "@material-ui/core";
 
 import ChatInput from './ChatInput'
 import ChatMessage from './ChatMessage'
 
+const URL = 'ws://localhost:3030'
+
 const Chat = () => {
     const [name, setName] = useState('')
     const [messages, setMessages] = useState([])
+
+    const ws = new WebSocket(URL)
+
+    const addMessage = (message) => {
+        setMessages(state => [...state, message])
+    }
+
+    useEffect(() => {
+        console.log('connected')
+    }, [ws.onopen])
+
+    useEffect(() => {
+        ws.onmessage = (evt) => {
+            const message = JSON.parse(evt.data)
+            addMessage(message)
+        }
+    }, [ws.onmessage])
+
+    useEffect(() => {
+        ws: new WebSocket(URL)
+    }, [ws.onclose])
 
     return (
         <div>
