1cd9e35d6ea91184ce740c3acf5e2f846c4268d8
jonfossheim <fossheimjoneven@gmail.com>
2020-05-14 14:24:56 +0200

    base component creation

---
+++ client/src/components/ChatInput.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ChatInput = () => {
+    return(
+        <>
+        </>
+    )
+}
+
+export default ChatInput
---
+++ client/src/components/ChatMessage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatMessage = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default ChatMessage
